06a7169347adf6c62dbe94bd0d81b569200ac01a
Your Name <mareliza16@hotmail.com>
2022-11-30 12:49:46 -0500

    Feat:the code is organized since there are some misspelled words and for that reason, it loaded a link that did not belong in the whatsapp button

--- react/components/WhatsappButton.tsx
+++ react/components/WhatsappButton.tsx
@@ -12,25 +12,22 @@ height: number
 const WhatsappButton= ({logo, phone, message, width, height }: Props) =>
 {
 const formattedMessage = message.replace(/  /g, "%20")
-console.log("Mi mensaje formateado es", formattedMessage, logo)
+
   return <>
 <div className='fixed bottom-2 right-2  flex  flexColum'>
-<a href={`https://wa.me/${phone}?text=Me%20interesa%20in%20el%20auto%20que%20vendes`}
+<a href={`https://wa.me/${phone}?text=${formattedMessage}`}
 target= "_blank"
 rel="noreferrer noopener"
 >
-  {logo}
-</a>
 <img src={logo}
 width={width}
 height={height}
-alt="logo de WhatsApp"
- />
+alt="logo de WhatsApp"  />
+</a>
 </div>
   </>
 }
-
-WhatsappButton.prototype={
+WhatsappButton.propTypes={
   logo: PropTypes.string.isRequired,
   phone: PropTypes.string.isRequired,
   message:  PropTypes.string,
@@ -42,8 +39,8 @@ WhatsappButton.defaultProps ={
   logo: "mi-logo.png",
  phone: "3004507575",
  message: "Estás comunicándote con VTEX University, por favor  ingresa tu duda",
-   width: 60,
-  height: 60
+   width: 50,
+  height: 50
 }
 
 WhatsappButton.schema = {
@@ -54,7 +51,7 @@ logo: {
 title: "Logo de WhatsApp que se relaciones con la marca",
 type: "string",
 widget: {
-"ui:widget": "image-uploader"
+"ui.widget": "image-uploader"
 }
 },
 phone:{
@@ -67,23 +64,20 @@ title: "Mensaje",
 description: "Agregar por favor el mensaje que se verá para tu cliente",
 type: "string",
 widget: {
-"ui:widget": "textarea"
+"ui.widget": "textarea"
 }
 },
 width:{
-title: "Teléfono",
-description: "Agregar por favor el número de teléfono",
+title: "Ancho",
+description: "con esta propiedad se le agrega el ancho a la imagen de whatsapp",
 type: "number"
 },
 height:{
-title: "Teléfono",
-description: "Agregar por favor el número de teléfono",
+title: "Alto",
+description: "con esta propiedad se le agrega el alto a la imagen de whatsapp",
 type: "number"
 },
 }
-
 }
-
-
 
 export default WhatsappButton;
